33bc69b36c27017ae048dee3669735d3456c994e
Alvaro <alvaro2014oliveira@gmail.com>
2020-10-01 20:03:26 -0300

    Novas animações

--- Assets/personagens/hill/Walk.cs
+++ Assets/personagens/hill/Walk.cs
@@ -7,6 +7,8 @@ public class Walk : MonoBehaviour
 	public float _rodar = 100;
 	private Animator _animator;
 	private float _andar = 0;
+	private float _pular = 0;
+	private float _direcao = 0;
 	
     // Start is called before the first frame update
     void Start()
@@ -26,12 +28,35 @@ public class Walk : MonoBehaviour
     	if (Input.GetKeyUp(KeyCode.LeftShift) && _andar > 0){
     		_andar = 1;
     	}
+    	if (Input.GetKey(KeyCode.Space)){
+    		_pular -= 1;
+    	}
+    	if (Input.GetKeyUp(KeyCode.Space)){
+    		_pular = 0;
+    	}
+
+
+    	if (Input.GetKey(KeyCode.D)){
+    		_direcao += 1;
+    	}
+    	if (Input.GetKeyUp(KeyCode.D)){
+    		_direcao = 0;
+    	}
+    	if (Input.GetKey(KeyCode.A)){
+    		_direcao -= 1;
+    	}
+    	if (Input.GetKeyUp(KeyCode.A)){
+    		_direcao = 0;
+    	}
+
 
         // if (transform.position.y > 0){
         // 	transform.position += Vector3.down * Time.deltaTime;
         // }
 
         _animator.SetFloat("Andar", _andar);
+        _animator.SetFloat("Pular", _pular);
+        _animator.SetFloat("Direcao", _direcao);
         
         this.transform.Rotate(0, (Input.GetAxis("Horizontal")* _rodar) * Time.deltaTime, 0);
     }
