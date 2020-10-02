b08f4e05592555442cb10d6b70386e662172d3a5
Alvaro <alvaro2014oliveira@gmail.com>
2020-10-01 21:52:31 -0300

    script de proximidade

---
+++ Assets/personagens/zombie/RunnerScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunnerScript : MonoBehaviour
+{
+	private Animator _animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+	public float _proximity = 0;
+    public float detectionRange;
+ 	public Transform amnesia;
+ 	public Transform hill;
+ 	private float timeLeft = 2.5f;
+ 	
+ 	void Update(){
+ 		
+ 		if( Vector3.Distance( amnesia.position, transform.position) <= detectionRange ){
+ 			_proximity += 1;
+ 			timeLeft -= Time.deltaTime;
+ 			if(timeLeft < 0) {	
+            	gameObject.SetActive(false);
+         	}
+ 		}
+ 		if( Vector3.Distance( hill.position, transform.position) <= detectionRange ){
+ 			_proximity += 1;
+ 			timeLeft -= Time.deltaTime;
+ 			if(timeLeft < 0) {	
+            	gameObject.SetActive(false);
+         	}
+ 		}
+
+ 		_animator.SetFloat("Proximity", _proximity);
+ 	}
+ 	
+}
